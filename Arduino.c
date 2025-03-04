59ff2b9954b1e00f26757c2da2864629f543cbf2
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2025-03-04 09:27:22 -0500

    Create Arduino.c

---
+++ Arduino.c
@@ -0,0 +1,34 @@
+/*
+  blink program with breadboard
+
+  created on Mar 2025
+  by yoochan Han
+*/
+
+int blinkTime = 1000; //set variable to 1000
+int PIN_11 = 11;
+int PIN_12 = 12;
+int PIN_13 = 13;
+
+void setup()
+{
+  pinMode(PIN_11, OUTPUT);
+  pinMode(PIN_12, OUTPUT);
+  pinMode(PIN_13, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(PIN_11, HIGH);
+  digitalWrite(PIN_12, LOW);
+  digitalWrite(PIN_13, LOW);
+  delay(1000); // Wait for a second
+  digitalWrite(PIN_11, LOW);
+  digitalWrite(PIN_12, HIGH);
+  digitalWrite(PIN_13, LOW);
+  delay(1000); // Wait for a second
+  digitalWrite(PIN_11, LOW);
+  digitalWrite(PIN_12, LOW);
+  digitalWrite(PIN_13, HIGH);
+  delay(1000); // Wait for a second
+}
